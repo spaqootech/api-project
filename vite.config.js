1d27ec30816755a6b86772c8636c5854c3e9a95a
Muhammad Ali <123479909+spaqootech@users.noreply.github.com>
2024-01-23 22:30:43 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['/src/main.jsx'], // This is incorrect
-      external: ['/src/main.jsx'], // Add the correct module name here
-    },
-  },
-})
+});
